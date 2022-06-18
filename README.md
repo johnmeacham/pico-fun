5abf251bd0f9b276f9e77e54d57a755919240c62
John <john@repetae.net>
2022-06-17 23:05:41 -0700

    add onoff.c

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # pico-fun
 Various projects with the raspberry pi pico
+
+## onoff.c
+
+use your reset button as a power switch. very useful for the battery powered
+lilygo models that don't have a physical way to disconnect the battery. You
+can turn off the periherals and shut down the raspberry pi pico clock.
+
+Just call the onoff() function at the beginning of your main program and it
+will let you toggle on and off your pico. It checks various hardware
+registers and a flag to make sure it was reset due to a power button and not
+due to initial power on (which may have garbage values in memory) or
+rebooting after programming it with the bootloader so it will automatically
+turn on when reprogrammed.
+
---
+++ onoff.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include "pico/stdlib.h"
+#include "pico/platform.h"
+#include "hardware/clocks.h"
+#include "hardware/sync.h"
+#include "hardware/xosc.h"
+#include "hardware/watchdog.h"
+#include "hardware/structs/vreg_and_chip_reset.h"
+//#include "pico/sleep.h"
+
+/* set a flag and use the reset button as an on off switch. Useful for battery
+ * powered devices without a physical switch.
+ *
+ * It will not treat it as a turning off button press if the reset was not due to
+ * pressing the run button nor if the watchdog timer scratch registers were used
+ * to override the normal boot sequence.
+ *
+ * Call this early in main. if you want usb picotool reset support, set up usb
+ * stdio before calling it.
+ *
+ * TODO(jwm): can we do better than wfi for power consumption?  Leaving on usb
+ * stdio for picotool support uses power.
+ */
+void onoff(void)
+{
+        static uint32_t __uninitialized_ram(flag); bool had_run =
+                vreg_and_chip_reset_hw->chip_reset &
+                VREG_AND_CHIP_RESET_CHIP_RESET_HAD_RUN_BITS; if
+        (!watchdog_hw->scratch[5] && had_run && flag == 42) {
+                flag = 0;
+//               clock_disable(clk_adc); xosc_dormant();
+                for (;;) __wfi();
+        } flag = 42;
+}
